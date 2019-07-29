ebc2b6519e16f8b30668599a26cab94ca2a36ea3
bdmin <wenmq47@gmail.com>
2019-07-30 01:01:44 +0800

    more colorful

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:core';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -30,6 +31,7 @@ class TodoPage extends StatefulWidget {
 
 class _TodoPageState extends State<TodoPage> {
   TodoList _todoList;
+  Color color = Colors.white;
 
   @override
   void initState() {
@@ -76,83 +78,92 @@ class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//      appBar: AppBar(
-//        title: Text('毛圈任务'),
-//        actions: <Widget>[
-//          new IconButton(
-//              icon: new Icon(Icons.refresh),
-//              onPressed: () {
-//                widget.storage.readTodoList().then((TodoList todoList) {
-//                  setState(() {
-//                    _todoList = todoList;
-//                  });
-//                });
-//              })
-//        ],
-//      ),
-//      backgroundColor: Colors.black26,
-      body: ListView.builder(
-        itemCount: _todoList.todoList.length,
-//        (_todoList != null && _todoList.todoList.length > 0)
-//            ? _todoList.todoList.length * 2
-//            : 0,
-        itemBuilder: (context, pageIndex) {
-          int index = pageIndex ~/ 2;
-          return Dismissible(
-            child: Card(
-              child: ListTile(
-                title: Text(
-                  _todoList.todoList[index].title,
-                  style: TextStyle(
-                    decoration: _todoList.todoList[index].done
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none,
+      backgroundColor: color,
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        child: ListView.builder(
+          itemCount: _todoList.todoList.length,
+          itemBuilder: (context, index) {
+            return Dismissible(
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    _todoList.todoList[index].title,
+                    style: TextStyle(
+                      decoration: _todoList.todoList[index].done
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
 //                        color: Colors.white,
+                    ),
+                  ),
+                  leading: Checkbox(
+                    value: _todoList.todoList[index].done,
+                    onChanged: (value) {
+                      _changeTodoState(index);
+                    },
+//                      checkColor: Colors.white,
+                    activeColor: Colors.deepOrange,
                   ),
                 ),
-                leading: Checkbox(
-                  value: _todoList.todoList[index].done,
-                  onChanged: (value) {
-                    _changeTodoState(index);
-                  },
-//                      checkColor: Colors.white,
-                  activeColor: Colors.deepOrange,
-                ),
               ),
-            ),
-            key: new Key(DateTime.now().toString() + pageIndex.toString()),
-            onDismissed: (direction) {
-              _deleteTodo(index);
-            },
-            confirmDismiss: (direction) async {
-              bool isConfirmed = true;
-              await showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Confirm Deletation'),
-                      content: Text('All task infomation will lose.'),
-                      actions: <Widget>[
-                        FlatButton(
-                          child: Text('Yes'),
-                          onPressed: () {
-                            isConfirmed = true;
-                            Navigator.pop(context);
-                          },
-                        ),
-                        FlatButton(
-                          child: Text('Cancel'),
-                          onPressed: () {
-                            isConfirmed = false;
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
-                    );
-                  });
-              return isConfirmed;
-            },
-          );
+              key: new Key(DateTime.now().toString() + index.toString()),
+              onDismissed: (direction) {
+                _deleteTodo(index);
+              },
+              confirmDismiss: (direction) async {
+                bool isConfirmed = true;
+                await showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Confirm Deletation'),
+                        content: Text('All task infomation will lose.'),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('Yes'),
+                            onPressed: () {
+                              isConfirmed = true;
+                              Navigator.pop(context);
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              isConfirmed = false;
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    });
+                return isConfirmed;
+              },
+            );
+          },
+        ),
+        onVerticalDragStart: (details) {
+          print("onVerticalDragStart" + details.toString());
+        },
+        onVerticalDragDown: (details) {
+          setState(() {
+            int colorValue =
+                (details.globalPosition.dy / 720 * 256 % 256).floor();
+            color = Color.fromARGB(
+                255, colorValue, 256 - colorValue, (colorValue - 128).abs());
+
+            print(colorValue);
+          });
+          print("onVerticalDragDown" + details.toString());
+        },
+        onVerticalDragCancel: () {
+          print("onVerticalDragCancel");
+          return null;
+        },
+        onVerticalDragUpdate: (details) {
+          print("onVerticalDragUpdate" + details.toString());
+        },
+        onVerticalDragEnd: (details) {
+          print("onVerticalDragEnd" + details.toString());
         },
       ),
       floatingActionButton: FloatingActionButton(
